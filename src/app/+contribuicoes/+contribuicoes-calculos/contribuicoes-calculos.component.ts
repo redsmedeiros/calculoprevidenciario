540b3ff530754f0dbfd7635b165ca1f102ca0e74
Felipe Dumont <felipe.dumont@codificar.com.br>
2018-03-15 13:30:34 -0300

    Alteracoes na tela de contribuicoes

--- src/app/+contribuicoes/+contribuicoes-calculos/contribuicoes-calculos.component.ts
+++ src/app/+contribuicoes/+contribuicoes-calculos/contribuicoes-calculos.component.ts
@@ -8,9 +8,72 @@ import {FadeInTop} from "../../shared/animations/fade-in-top.decorator";
 })
 export class ContribuicoesCalculosComponent implements OnInit {
 
-  public styleTheme: string = 'style-0';
 
-  public styleThemes: Array<string> = ['style-0', 'style-1', 'style-2', 'style-3'];
+
+
+  public state: any = {
+    tabs: {
+      demo1: 0,
+      demo2: 'tab-r1',
+      demo3: 'hr1',
+      demo4: 'AA',
+      demo5: 'iss1',
+      demo6: 'l1',
+      demo7: 'tab1',
+      demo8: 'hb1',
+      demo9: 'A1',
+      demo10: 'is1'
+    },
+
+    carousel: {
+      demo1: {
+        interval: 2000,
+        noWrap: false,
+        slides: [
+          {
+            title: 'Title 1',
+            text: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.',
+            src: 'assets/img/demo/m3.jpg',
+          },
+          {
+            title: 'Title 2',
+            text: 'Dolores justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.',
+            src: 'assets/img/demo/m2.jpg',
+          },
+          {
+            title: 'Title 3',
+            text: 'Lorem justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.',
+            src: 'assets/img/demo/m1.jpg',
+          },
+        ]
+      },
+      demo2: {
+        interval: 3000,
+        noWrap: false,
+        slides: [
+          {
+            title: 'Title 2',
+            text: 'Dolores justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.',
+            src: 'assets/img/demo/m2.jpg',
+          },
+          {
+            title: 'Title 1',
+            text: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.',
+            src: 'assets/img/demo/m3.jpg',
+          },
+          {
+            title: 'Title 3',
+            text: 'Lorem justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.',
+            src: 'assets/img/demo/m1.jpg',
+          },
+        ]
+      }
+    }
+  };
+
+
+  
+
 
   constructor() {}
 
