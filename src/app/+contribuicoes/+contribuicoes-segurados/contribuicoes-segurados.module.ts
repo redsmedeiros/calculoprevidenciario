c802e59e9d4ce19f9a0576b62ff76771e9aeba10
Rafael Bordin <rafael.bordin@live.com>
2018-05-09 23:21:46 -0300

    Add segurado form

--- src/app/+contribuicoes/+contribuicoes-segurados/contribuicoes-segurados.module.ts
+++ src/app/+contribuicoes/+contribuicoes-segurados/contribuicoes-segurados.module.ts
@@ -4,13 +4,13 @@ import { contribuicoesSeguradosRouting } from './contribuicoes-segurados.routing
 import {ContribuicoesSeguradosComponent} from "./contribuicoes-segurados.component";
 import {SmartadminModule} from "../../shared/smartadmin.module";
 import {SmartadminDatatableModule} from "../../shared/ui/datatable/smartadmin-datatable.module";
-
 import { ContribuicoesSeguradosFormComponent } from './contribuicoes-segurados-form/contribuicoes-segurados-form.component';
 import { ContribuicoesSeguradosDestroyComponent } from './contribuicoes-segurados-destroy/contribuicoes-segurados-destroy.component';
 import { ContribuicoesSeguradosIndexComponent } from './contribuicoes-segurados-index/contribuicoes-segurados-index.component';
 import { ContribuicoesSeguradosCreateComponent } from './contribuicoes-segurados-create/contribuicoes-segurados-create.component';
 import { ContribuicoesSeguradosEditComponent } from './contribuicoes-segurados-edit/contribuicoes-segurados-edit.component';
-
+import {I18nModule} from "../../shared/i18n/i18n.module";
+import { TextMaskModule } from 'angular2-text-mask';
 
 
 @NgModule({
@@ -18,13 +18,12 @@ import { ContribuicoesSeguradosEditComponent } from './contribuicoes-segurados-e
     CommonModule,
     contribuicoesSeguradosRouting,
     SmartadminModule,
+    TextMaskModule,
+    I18nModule,
     SmartadminDatatableModule
   ],
-  declarations: [ContribuicoesSeguradosComponent,
-  					ContribuicoesSeguradosFormComponent,
-  					ContribuicoesSeguradosDestroyComponent,
-  					ContribuicoesSeguradosIndexComponent,
-  					ContribuicoesSeguradosCreateComponent,
-  					ContribuicoesSeguradosEditComponent]
+  declarations: [
+    ContribuicoesSeguradosComponent, ContribuicoesSeguradosFormComponent, ContribuicoesSeguradosDestroyComponent, ContribuicoesSeguradosIndexComponent, ContribuicoesSeguradosCreateComponent, ContribuicoesSeguradosEditComponent
+  ]
 })
 export class ContribuicoesSeguradosModule { }
