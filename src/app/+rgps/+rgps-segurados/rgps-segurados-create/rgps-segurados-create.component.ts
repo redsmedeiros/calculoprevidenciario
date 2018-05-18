8b432c00a67f71014c93203d96ac5b3dc38d6405
Rafael Bordin <rafael.bordin@live.com>
2018-05-18 11:09:42 -0300

    Componente Create

--- src/app/+rgps/+rgps-segurados/rgps-segurados-create/rgps-segurados-create.component.ts
+++ src/app/+rgps/+rgps-segurados/rgps-segurados-create/rgps-segurados-create.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { SeguradoService } from '../SeguradoRgps.service';
+import { ErrorService } from '../../../services/error.service';
+import { SeguradoRgps as SeguradoModel } from '../SeguradoRgps.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-rgps-segurados-create',
   templateUrl: './rgps-segurados-create.component.html',
-  styleUrls: ['./rgps-segurados-create.component.css']
+  styleUrls: ['./rgps-segurados-create.component.css'],
+  providers: [
+    ErrorService
+  ]
 })
-export class RgpsSeguradosCreateComponent implements OnInit {
+export class RgpsSeguradosCreateComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public styleTheme = 'style-0';
+  public styleThemes: Array<string> = ['style-0', 'style-1', 'style-2', 'style-3'];
+
+  public form = {...SeguradoModel.form};
+  @Output() onSubmit = new EventEmitter();
+
+  constructor(
+    protected Segurado: SeguradoService,
+    protected Errors: ErrorService,
+    protected router: Router,
+  ) {}
 
   ngOnInit() {
+  }
+
+  submit(data) {
+    this.Segurado
+          .save(data)
+          .then(model => {
+            this.resetForm();
+            this.onSubmit.emit();
+          })
+          .catch(errors => this.Errors.add(errors));
+  }
+
+  ngOnDestroy() {
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.form = {...SeguradoModel.form};
   }
 
 }
