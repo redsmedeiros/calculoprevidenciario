9989e7b36660a477b37fbca1fef978d9592fe50b
Rafael Bordin <rafael.bordin@live.com>
2018-05-19 18:20:38 -0300

    Componente Create

--- src/app/+rgps/+rgps-calculos/rgps-calculos-create/rgps-calculos-create.component.ts
+++ src/app/+rgps/+rgps-calculos/rgps-calculos-create/rgps-calculos-create.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { CalculoRgpsService } from '../CalculoRgps.service';
+import { ErrorService } from '../../../services/error.service';
+import { CalculoRgps as CalculoModel } from '../CalculoRgps.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-rgps-calculos-create',
   templateUrl: './rgps-calculos-create.component.html',
-  styleUrls: ['./rgps-calculos-create.component.css']
+  styleUrls: ['./rgps-calculos-create.component.css'],
+  providers: [
+    ErrorService
+  ]
 })
-export class RgpsCalculosCreateComponent implements OnInit {
+export class RgpsCalculosCreateComponent implements OnDestroy {
+  public styleTheme = 'style-0';
+  public styleThemes: Array<string> = ['style-0', 'style-1', 'style-2', 'style-3'];
+  public form = {...CalculoModel.form};
 
-  constructor() { }
+  @Output() onSubmit = new EventEmitter();
 
-  ngOnInit() {
+  constructor(    
+  	protected Calculo: CalculoRgpsService,
+    protected Errors: ErrorService,
+    protected router: Router
+    ) { }
+
+  submit(data) {
+    this.Calculo
+          .save(data)
+          .then(model => {
+            this.resetForm();
+            this.onSubmit.emit();
+          })
+          .catch(errors => this.Errors.add(errors));
+  }
+
+
+  ngOnDestroy() {
+  	this.resetForm();
+  }
+
+  resetForm() {
+    this.form = {...CalculoModel.form};
   }
 
 }
