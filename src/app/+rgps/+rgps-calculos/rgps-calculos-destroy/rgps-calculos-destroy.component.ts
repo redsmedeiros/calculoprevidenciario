ad86b5c03bc07eae44d2d4def02b5dc1f531b587
Rafael Bordin <rafael.bordin@live.com>
2018-05-20 20:00:59 -0300

    Componenete Destroy

--- src/app/+rgps/+rgps-calculos/rgps-calculos-destroy/rgps-calculos-destroy.component.ts
+++ src/app/+rgps/+rgps-calculos/rgps-calculos-destroy/rgps-calculos-destroy.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FadeInTop } from '../../../shared/animations/fade-in-top.decorator';
+import { CalculoRgpsService } from '../CalculoRgps.service';
 
+import swal from 'sweetalert';
+
+
+@FadeInTop()
 @Component({
   selector: 'app-rgps-calculos-destroy',
   templateUrl: './rgps-calculos-destroy.component.html',
   styleUrls: ['./rgps-calculos-destroy.component.css']
 })
-export class RgpsCalculosDestroyComponent implements OnInit {
+export class RgpsCalculosDestroyComponent {
 
-  constructor() { }
+  constructor(
+    protected Calculo: CalculoRgpsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.Calculo.find(this.route.snapshot.params['id_calculo'])
+        .then(calculo => {
+          this.Calculo.destroy(calculo)
+              .then(() => this.router.navigate(['/rgps/rgps-segurados']));
+        })
+    swal(
+      'Sucesso',
+      'Calculo excluído',
+      'success'
+    )
 
-  ngOnInit() {
   }
+
 
 }
--- src/app/+rgps/+rgps-calculos/rgps-calculos.routing.ts
+++ src/app/+rgps/+rgps-calculos/rgps-calculos.routing.ts
@@ -2,14 +2,22 @@
 import {ModuleWithProviders} from "@angular/core"
 import { Routes, RouterModule } from '@angular/router';
 import {RgpsCalculosComponent} from "./rgps-calculos.component";
+import { RgpsCalculosDestroyComponent } from './rgps-calculos-destroy/rgps-calculos-destroy.component';
 
-export const rgpsCalculosRoutes: Routes = [{
-  path: '',
-  component: RgpsCalculosComponent
-},{
-  path: ':id',
-  component: RgpsCalculosComponent
-}];
+export const rgpsCalculosRoutes: Routes = [
+	{
+  		path: '',
+  		component: RgpsCalculosComponent
+	},
+	{
+  		path: ':id',
+  		component: RgpsCalculosComponent
+	},
+	{
+    	path: ':id/:id_calculo/destroy',
+    	component: RgpsCalculosDestroyComponent
+  	}
+  ];
 
 export const rgpsCalculosRouting = RouterModule.forChild(rgpsCalculosRoutes);
 
