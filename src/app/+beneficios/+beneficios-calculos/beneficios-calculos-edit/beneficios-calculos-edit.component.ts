983529b71c5322681be40208f3077cf7e14b9155
Rafael Bordin <rafael.bordin@live.com>
2018-05-23 13:17:02 -0300

    Componente Edit

--- src/app/+beneficios/+beneficios-calculos/beneficios-calculos-edit/beneficios-calculos-edit.component.ts
+++ src/app/+beneficios/+beneficios-calculos/beneficios-calculos-edit/beneficios-calculos-edit.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CalculoAtrasadoService } from '../CalculoAtrasado.service';
+import { ErrorService } from '../../../services/error.service';
+import { CalculoAtrasado as CalculoModel } from '../CalculoAtrasado.model';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-beneficios-calculos-edit',
   templateUrl: './beneficios-calculos-edit.component.html',
   styleUrls: ['./beneficios-calculos-edit.component.css']
 })
-export class BeneficiosCalculosEditComponent implements OnInit {
+export class BeneficiosCalculosEditComponent implements OnInit, OnDestroy {
+  public edit_mode = true;
+  
+  public styleTheme = 'style-0';
+  public styleThemes: Array<string> = ['style-0', 'style-1', 'style-2', 'style-3'];
 
-  constructor() { }
+  public form = {...CalculoModel.form};
+  public calculo;
+
+  public isUpdating = false;
+  constructor(
+  	protected CalculoAtrasado: CalculoAtrasadoService,
+    protected Errors: ErrorService,
+    protected router: Router,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
+  		this.isUpdating = true;
+  	    this.CalculoAtrasado.find(this.route.snapshot.params['id_calculo'])
+          .then(calculo => {
+            this.calculo = calculo;
+            this.form = this.calculo;
+            this.isUpdating = false;
+          });
+  	console.log(this.form);
+  }
+
+
+  submit(data) {
+    	this.CalculoAtrasado
+          .update(this.calculo)
+          .then(model => {
+            this.CalculoAtrasado.get()
+                .then(() => this.router.navigate(['/beneficios/beneficios-calculos/'+this.route.snapshot.params['id']]));
+          })
+          .catch(errors => this.Errors.add(errors));
+  }
+
+  ngOnDestroy(){
+  	this.resetForm();
+    if (!this.Errors.empty()) {
+      Object.keys(this.Errors.all()).forEach(field => {
+        this.calculo[field] = this.calculo['_data'][field];
+      });
+      this.Errors.clear();
+    }
+
+  }
+
+  resetForm() {
+    this.form = {...CalculoModel.form};
   }
 
 }
--- src/app/+beneficios/+beneficios-calculos/beneficios-calculos.routing.ts
+++ src/app/+beneficios/+beneficios-calculos/beneficios-calculos.routing.ts
@@ -3,16 +3,23 @@ import {ModuleWithProviders} from '@angular/core'
 import { Routes, RouterModule } from '@angular/router';
 import {BeneficiosCalculosComponent} from './beneficios-calculos.component';
 import {BeneficiosCalculosDestroyComponent} from './beneficios-calculos-destroy/beneficios-calculos-destroy.component';
-
-export const beneficiosCalculosRoutes: Routes = [{
+import {BeneficiosCalculosEditComponent} from './beneficios-calculos-edit/beneficios-calculos-edit.component';
+export const beneficiosCalculosRoutes: Routes = [
+{
   path: '',
   component: BeneficiosCalculosComponent
-},{
+},
+{
   path: ':id',
   component: BeneficiosCalculosComponent
-},{
+},
+{
     path: ':id/:id_calculo/destroy',
     component: BeneficiosCalculosDestroyComponent
+},
+{
+    path: ':id/:id_calculo/edit',
+    component: BeneficiosCalculosEditComponent
 }
 ];
 
