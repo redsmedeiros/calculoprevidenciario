05a86616baf95f9b74606c9cba4bbaf0a980f280
Rafael Bordin <rafael.bordin@live.com>
2018-06-09 19:31:38 -0300

    Resultados estaticos exibidos a partir do calculo

--- src/app/+beneficios/+beneficios-resultados/beneficios-resultados.component.ts
+++ src/app/+beneficios/+beneficios-resultados/beneficios-resultados.component.ts
@@ -18,13 +18,16 @@ export class BeneficiosResultadosComponent implements OnInit {
   public styleThemes: Array<string> = ['style-0', 'style-1', 'style-2', 'style-3'];
 
   public segurado:any = {};
+  public calculo:any = {};
 
   public isUpdating = false;
 
 
-  constructor(protected Segurado: SeguradoService,
-              protected router: Router,
-              private route: ActivatedRoute) {}
+  constructor(protected router: Router,
+              private route: ActivatedRoute,
+              protected Segurado: SeguradoService,
+              protected CalculoAtrasado: CalculoService
+              ) {}
 
   ngOnInit() {
     this.isUpdating = true;
@@ -32,8 +35,100 @@ export class BeneficiosResultadosComponent implements OnInit {
     this.Segurado.find(this.route.snapshot.params['id'])
         .then(segurado => {
             this.segurado = segurado;
-            this.isUpdating = false;
     });
+
+    this.CalculoAtrasado.find(this.route.snapshot.params['id_calculo'])
+    	.then(calculo => {
+    		this.calculo = calculo;
+    		console.log(calculo);
+    		this.isUpdating = false;
+    });
+
+  }
+
+  formatDatetimeToDate(dataString){
+  	let date = dataString.split(' ')[0];
+  	let splited_date = date.split('-');
+  	return splited_date[2] + '/' +splited_date[1] + '/' + splited_date[0];
+  }
+
+  formatDate(dataString){
+  	if(dataString != '0000-00-00'){
+  	  	let splited_date = dataString.split('-');
+  	  	return splited_date[2] + '/' +splited_date[1] + '/' + splited_date[0];
+  	}
+  	return '--'
+  }
+
+  formatPercent(value){
+  	value = parseFloat(value) * 100;
+  	return this.formatDecimal(value, 0) + '%';
+  }
+
+  formatMoney(value){
+  	return 'R$' + this.formatDecimal(value, 2);
+  }
+
+  formatDecimal(value, n_of_decimal_digits){
+  	value = parseFloat(value);
+  	return (value.toFixed(parseInt(n_of_decimal_digits))).replace('.', ',');
+  }
+  getTipoAposentadoria(value){
+  	console.log(value);
+  	let tipos_aposentadoria = [{
+  	            name: "Auxílio Doença",
+  				value: 0
+  			},{
+  				name: "Aposentadoria por invalidez Previdenciária ou Pensão por Morte",
+  				value: 1
+  			},{
+  				name: "Aposentadoria por idade - Trabalhador Urbano",
+  				value: 2
+  			},{
+  				name: "Aposentadoria por tempo de contribuição",
+  				value: 3
+  			},{
+  				name: "Aposentadoria por tempo de serviço de professor",
+  				value: 4
+  			},{
+  				name: "Auxílio Acidente previdenciário - 50%",
+  				value: 5
+  			},{
+  				name: "Aposentadoria por idade - Trabalhador Rural",
+  				value: 6
+  			},{
+  				name: "Auxílio Acidente  - 30%",
+  				value: 7
+  			},{
+  				name: "Auxílio Acidente - 40%",
+  				value: 8
+  			},{
+  				name: "Auxílio Acidente - 60%",
+  				value: 9
+  			},{
+  				name: "Abono de Permanência em Serviço",
+  				value: 10
+  			},{
+  				name: "LOAS - Benefício no valor de um salário mínimo",
+  				value: 11
+  			},{
+  				name: "Aposentadoria especial da Pessoa com Deficiência Grave",
+  				value: 12
+  			},{
+  				name: "Aposentadoria especial da Pessoa com Deficiência Moderada",
+  				value: 13
+  			},{
+  				name: "Aposentadoria especial da Pessoa com Deficiência Leve",
+  				value: 14
+  			},{
+  				name: "Aposentadoria especial por Idade da Pessoa com Deficiência",
+  				value: 15
+  			},{
+  				name: "LOAS",
+  				value: 16
+  			}
+  	]
+  	return tipos_aposentadoria[value].name;
   }
 
   editSegurado() {
