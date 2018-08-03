0f7c72dda4149d5b44f6e3e322bb1cdfad457d6b
Rafael Bordin <rafael.bordin@live.com>
2018-08-03 00:26:09 -0300

    Filtragem dos valores contribuidos

--- src/app/+rgps/+rgps-valores-contribuidos/ValorContribuido.service.ts
+++ src/app/+rgps/+rgps-valores-contribuidos/ValorContribuido.service.ts
@@ -1,11 +1,37 @@
 import { ControllerService } from '../../contracts/Controller.service';
 
 import { ValorContribuido } from './ValorContribuido.model';
+import * as moment from 'moment';
 
 export class ValorContribuidoService extends ControllerService {
 
   public model = ValorContribuido;
   public name = 'valorContribuido';
   public list: ValorContribuido[] = this.store.data['valorContribuido'];
+
+  public getByCalculoId(calculoId, dataInicio, dataLimite) {
+
+    return new Promise((resolve, reject) => {
+    	let parameters = ['id_calculo', calculoId, 'inicio_intervalo', dataInicio.format('YYYY-MM-DD')];
+	  	if (this.list.length == 0) {
+	  		this.getWithParameters(parameters).then(() => {
+			  	let list = this.list.filter((valor:ValorContribuido) => {
+			  		let dataContribuicao = moment(valor.data);
+			  		return dataContribuicao <= dataInicio &&  dataContribuicao >= dataLimite;
+			  	});
+			  	resolve(list);
+	  		}).catch(error => {
+	          console.error(error);
+	          reject(error);	  			
+	  		})
+	  	} else {
+			let list = this.list.filter((valor:ValorContribuido) => {
+				let dataContribuicao = moment(valor.data);
+			  return dataContribuicao <= dataInicio &&  dataContribuicao >= dataLimite;
+			});
+	  		resolve(list);
+  		}
+    });
+  }
 
 }
