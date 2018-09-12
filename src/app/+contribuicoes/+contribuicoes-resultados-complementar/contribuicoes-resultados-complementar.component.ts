eac029c056a8463c6e318f071397d8f31e93556e
Rafael Bordin <rafael.bordin@live.com>
2018-09-12 19:51:44 -0300

    issue 6328

--- src/app/+contribuicoes/+contribuicoes-resultados-complementar/contribuicoes-resultados-complementar.component.ts
+++ src/app/+contribuicoes/+contribuicoes-resultados-complementar/contribuicoes-resultados-complementar.component.ts
@@ -238,6 +238,7 @@ export class ContribuicoesResultadosComplementarComponent implements OnInit {
   }
 
   formatMoney(data){
+    data = parseFloat(data);
     return 'R$ ' + (data.toFixed(2)).replace('.',',');
   }
 
