64dbb5bdb63de20fab0176ea46c8b4853bbe1c11
Rafael Bordin <rafael.bordin@live.com>
2018-10-10 14:45:20 -0300

    issue 6588

--- src/app/+beneficios/+beneficios-segurados/beneficios-segurados-create/beneficios-segurados-create.component.ts
+++ src/app/+beneficios/+beneficios-segurados/beneficios-segurados-create/beneficios-segurados-create.component.ts
@@ -33,10 +33,10 @@ export class BeneficiosSeguradosCreateComponent implements OnInit, OnDestroy {
   submit(data) {
     this.Segurado
           .save(data)
-          .then(model => {
+          .then((model:SeguradoModel) => {
             this.resetForm();
             this.onSubmit.emit();
-            console.log(model)
+            window.location.href='#/beneficios/beneficios-calculos/'+ model.id;
           })
           .catch(errors => this.Errors.add(errors));
   }
