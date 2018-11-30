c508da85b117631fdbe824d8858f51576a60bb64
Rafael Bordin <rafael.bordin@live.com>
2018-11-30 18:25:56 -0200

    issue 7450

--- src/app/+beneficios/+beneficios-resultados/beneficios-resultados.component.ts
+++ src/app/+beneficios/+beneficios-resultados/beneficios-resultados.component.ts
@@ -1188,7 +1188,8 @@ export class BeneficiosResultadosComponent implements OnInit {
 
     if (maturidade != 0) {
       if (this.calculo.data_cessacao != '0000-00-00') { // verifica se o calculo possui data de cessacao
-        somaVincendos = this.ultimoBeneficioDevidoAntesProporcionalidade * maturidade; //Beneficio Devido da ultima linha antes da aplicação da proporcionalidade.
+        //somaVincendos = this.ultimoBeneficioDevidoAntesProporcionalidade * maturidade; //Beneficio Devido da ultima linha antes da aplicação da proporcionalidade.
+        somaVincendos = this.ultimaDiferencaMensal * maturidade; //Diferneça mensal da ultima linha antes da aplicação da proporcionalidade.
       }else{
         if (somaVincendos < 0) {
           somaVincendos = this.ultimaDiferencaMensal * maturidade;
