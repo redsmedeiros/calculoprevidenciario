c8228087825d0fad1533c3339c162e48e193d3b1
Rafael Bordin <rafael.bordin@live.com>
2019-01-28 19:01:43 -0200

    teste

--- src/app/can-activate-route.guard.ts
+++ src/app/can-activate-route.guard.ts
@@ -10,16 +10,16 @@ import swal from 'sweetalert';
 export class OnlyLoggedInUsersGuard implements CanActivate, CanActivateChild {
   constructor(private Auth: Auth , private router: Router) {}
 
-  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):Promise<boolean> {
     let params = route.queryParams;
     let product = localStorage.getItem('product') || params.product;
     let type = localStorage.getItem('type') || params.type;
     let user_id = params.user_id || localStorage.getItem('user_id');
     let user_token = localStorage.getItem('user_token') || params.user_token;
-    
+
     return new Promise((resolve) => {
       if (user_id && user_token) {
-
+        
         //Caso haja parametros via query string, limpa a url
         if(!(Object.keys(params).length === 0 && params.constructor === Object)){
           localStorage.setItem('user_id', user_id);
@@ -41,20 +41,68 @@ export class OnlyLoggedInUsersGuard implements CanActivate, CanActivateChild {
         }).catch(err => {
           if(err.response.status == 401){
             //redirecionar para pagina de login
-            swal('Erro', 'É necessário estar logado para acessar esta página.', 'error').then(()=> {
+            // swal('Erro', 'É necessário estar logado para acessar esta página.', 'error').then(()=> {
+            swal('Erro', 'CAC: Não autorizado. Token expirado', 'error').then(()=> {
               window.location.href = environment.loginPageUrl;
             });
             resolve(false);
           }
         });
       } else {
-        swal('Erro', 'É necessário estar logado para acessar esta página.', 'error').then(()=> {
+        //swal('Erro', 'É necessário estar logado para acessar esta página.', 'error').then(()=> {
+        swal('Erro', 'CAC: Sem user_id e sem user_token.', 'error').then(()=> {
               window.location.href = environment.loginPageUrl;
             });
         resolve(false);
       }
     });
   }
+
+  // canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+  //   let params = route.queryParams;
+  //   let product = localStorage.getItem('product') || params.product;
+  //   let type = localStorage.getItem('type') || params.type;
+  //   let user_id = params.user_id || localStorage.getItem('user_id');
+  //   let user_token = localStorage.getItem('user_token') || params.user_token;
+    
+  //   return new Promise((resolve) => {
+  //     if (user_id && user_token) {
+
+  //       //Caso haja parametros via query string, limpa a url
+  //       if(!(Object.keys(params).length === 0 && params.constructor === Object)){
+  //         localStorage.setItem('user_id', user_id);
+  //         localStorage.setItem('user_token', user_token);
+  //         localStorage.setItem('product', product);
+  //         localStorage.setItem('type', type);
+  //         this.router.navigate(['.'], {queryParams: {}, queryParamsHandling: "merge",});
+  //         resolve(false)
+  //       }
+
+  //       this.Auth.authenticate(user_id, user_token, product, type).then((response:AuthResponse) => {
+  //         if(response.status){
+  //           localStorage.setItem('user_id', user_id);
+  //           localStorage.setItem('user_token', user_token);
+  //           localStorage.setItem('product', product);
+  //           localStorage.setItem('type', type);
+  //           resolve(true);
+  //         }
+  //       }).catch(err => {
+  //         if(err.response.status == 401){
+  //           //redirecionar para pagina de login
+  //           swal('Erro', 'É necessário estar logado para acessar esta página.', 'error').then(()=> {
+  //             window.location.href = environment.loginPageUrl;
+  //           });
+  //           resolve(false);
+  //         }
+  //       });
+  //     } else {
+  //       swal('Erro', 'É necessário estar logado para acessar esta página.', 'error').then(()=> {
+  //             window.location.href = environment.loginPageUrl;
+  //           });
+  //       resolve(false);
+  //     }
+  //   });
+  // }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):Promise<boolean> {
     let params = route.queryParams;
