3fd06a0463dd2a61c5609df41a340448799d550c
Desenvolvimento <desenvolvimento@iprev.com.br>
2021-03-05 16:35:49 -0300

    Add negativo antes do numero mascara

--- src/app/+beneficios/+beneficios-calculos/beneficios-calculos.module.ts
+++ src/app/+beneficios/+beneficios-calculos/beneficios-calculos.module.ts
@@ -11,20 +11,35 @@ import { BeneficiosCalculosFormComponent } from './beneficios-calculos-form/bene
 import { BeneficiosCalculosIndexComponent } from './beneficios-calculos-index/beneficios-calculos-index.component';
 import {I18nModule} from '../../shared/i18n/i18n.module';
 import { TextMaskModule } from 'angular2-text-mask';
-import { CurrencyMaskModule } from 'ng2-currency-mask';
-import { CurrencyMaskConfig, CURRENCY_MASK_CONFIG } from 'ng2-currency-mask/src/currency-mask.config';
+// import { CurrencyMaskModule } from 'ng2-currency-mask';
+// import { CurrencyMaskConfig, CURRENCY_MASK_CONFIG } from 'ng2-currency-mask/src/currency-mask.config';
+
+import { CurrencyMaskModule } from '@mvpproperties/ng2-currency-mask';
+import { CurrencyMaskConfig, CURRENCY_MASK_CONFIG } from '@mvpproperties/ng2-currency-mask/src/currency-mask.config';
+
 import { BeneficiosCalculosFormRecebidosComponent } from './beneficios-calculos-form-recebidos/beneficios-calculos-form-recebidos.component';
 import { BeneficiosCalculosFormDevidosComponent } from './beneficios-calculos-form-devidos/beneficios-calculos-form-devidos.component';
 import { BeneficiosCalculosFormCustasProcessoComponent } from './beneficios-calculos-form-custas-processo/beneficios-calculos-form-custas-processo.component';
  
+// export const CustomCurrencyMaskConfig: CurrencyMaskConfig = {
+//     align: 'left',
+//     allowNegative: false,
+//     decimal: ',',
+//     precision: 2,
+//     prefix: '',
+//     suffix: '',
+//     thousands: '.'
+// };
+
 export const CustomCurrencyMaskConfig: CurrencyMaskConfig = {
-    align: 'left',
-    allowNegative: false,
-    decimal: ',',
-    precision: 2,
-    prefix: '',
-    suffix: '',
-    thousands: '.'
+  align: 'right',
+  allowNegative: false,
+  allowNegativeZero: false,
+  decimal: ',',
+  precision: 2,
+  prefix: 'R$ ',
+  suffix: '',
+  thousands: '.'
 };
 
 @NgModule({
