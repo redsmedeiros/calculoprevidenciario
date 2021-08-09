41d97fc60ca14255c3122f8ab23deb43d1bbeda2
Desenvolvimento <desenvolvimento@iprev.com.br>
2021-08-09 12:26:07 -0300

    Fix - idade no ultimo periodo trabalhado

--- src/app/+contagem-tempo/+contagem-tempo-resultados/contagem-tempo-conclusao-segurado/contagem-tempo-conclusao-segurado.component.ts
+++ src/app/+contagem-tempo/+contagem-tempo-resultados/contagem-tempo-conclusao-segurado/contagem-tempo-conclusao-segurado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChange, OnChanges } from '@angular/core';
 
 
 @Component({
@@ -6,12 +6,13 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './contagem-tempo-conclusao-segurado.component.html',
   styleUrls: ['./contagem-tempo-conclusao-segurado.component.css']
 })
-export class ContagemTempoConclusaoSeguradoComponent implements OnInit {
+export class ContagemTempoConclusaoSeguradoComponent implements OnInit, OnChanges {
 
   @Input() calculo;
   @Input() segurado;
   @Input() isUpdating;
   @Input() lastdateNascimento;
+  private islastdateNascimento = false;
 
   public Math = Math;
 
@@ -20,20 +21,46 @@ export class ContagemTempoConclusaoSeguradoComponent implements OnInit {
 
   ngOnInit() { }
 
-  
+
+  ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
+
+    let lastdateNascimento = changes['lastdateNascimento'];
+    this.setIslastdateNascimento(this.lastdateNascimento)
+  }
+
+  private setIslastdateNascimento(lastdateNascimento) {
+    this.islastdateNascimento = false;
+
+    if (this.isExits(lastdateNascimento)
+      && this.isExits(lastdateNascimento.fullDays)
+      && lastdateNascimento.fullDays > 0) {
+      this.islastdateNascimento = true;
+    }
+
+  }
+
+
+  private isExits(value) {
+    return (typeof value !== 'undefined' &&
+      value != null && value !== 'null' &&
+      value !== undefined && value !== '')
+      ? true : false;
+  }
+
+
   private formateStringAnosMesesDias(anos, meses, dias, notDays = false) {
 
     if (notDays) {
-        return ` ${anos} ano(s), ${meses} mes(es)`;
+      return ` ${anos} ano(s), ${meses} mes(es)`;
     }
 
     if (anos < 0) {
-        return ` ${meses} mes(es) e ${Math.floor(dias)} dia(s)`;
+      return ` ${meses} mes(es) e ${Math.floor(dias)} dia(s)`;
     }
 
     return ` ${anos} ano(s), ${meses} mes(es) e ${Math.floor(dias)} dia(s)`;
 
-}
+  }
 
   editSegurado() {
     sessionStorage.setItem('last_url', '/contagem-tempo/contagem-tempo-resultados/' + this.segurado.id + '/' + this.calculo.id);
