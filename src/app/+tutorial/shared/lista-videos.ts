5338df94ba2f67e11e7ac4bd3b12d52793253690
Desenvolvimento <desenvolvimento@iprev.com.br>
2021-08-31 11:06:59 -0300

    Fix - videos youtube 32-34-36-37-39

--- src/app/+tutorial/shared/lista-videos.ts
+++ src/app/+tutorial/shared/lista-videos.ts
@@ -2,34 +2,44 @@ export class ListaVideos {
 
  public static listVideos = [{
     label: 'MIGRANDO OS DADOS DO CNIS PARA CONTAGEM DE TEMPO DE CONTRIBUIÇÃO ',
-    value: '453291034'
+    // value: '453291034'
+    value: '0wwPYtCh4lM'
   }, {
     label: 'MIGRANDO OS SALÁRIOS DE CONTRIBUIÇÃO DO CNIS',
-    value: '454141361'
+    value: 'tLHpO9tjuvQ'
+    // value: '454141361'
   }, {
     label: 'RMI DA APOSENTADORIA POR IDADE',
-    value: '453291169'
+    // value: '453291169'
+    value: 'r76CE45av2Q'
   }, {
     label: 'RMI DA APOSENTADORIA POR TEMPO DE CONTRIBUIÇÃO',
-    value: '453291229'
+    // value: '453291229'
+    value: '9e6XRlvbZ0g'
   }, {
     label: 'RMI DO AUXÍLIO POR INCAPACIDADE TEMPORÁRIA ',
-    value: '453291500'
+    value: 'vg-xS8r3D08'
+    // value: '453291500'
   }, {
     label: 'RMI DO APOSENTADORIA POR INCAPACIDADE PERMANENTE ',
-    value: '453291654'
+    // value: '453291654'
+    value: 'mPt8VhbFhNU'
   }, {
     label: 'RMI DO AUXÍLIO ACIDENTE',
-    value: '453291748'
+    // value: '453291748'
+    value: '6icZhmC9IDo'
   }, {
     label: 'RMI DA PENSÃO POR MORTE',
-    value: '453291755'
+    // value: '453291755'
+    value: 'y8H0tll1EuQ'
   }, {
     label: 'RMI DA APOSENTADORIA DA PcD',
-    value: '453291879'
+    // value: '453291879'
+    value: '-Ge9o59vWMI'
   }, {
     label: 'Revisão da Vida Toda - Dr. Sergio Geromes',
-    value: '413680168'
+    // value: '413680168'
+    value: 'jbUWfP6WYTo'
   }, {
     label: 'Liquidação de Sentença',
     value: '447520641'
